ab5a450e8aeade34f122ff7a9883099fa548c35f
Subha-P-Coder <psubha678@gmail.com>
2025-09-23 10:47:06 +0530

    feat:add typecasting in 3typeCasting.py

---
+++ 3typeCasting.py
@@ -0,0 +1,20 @@
+# type casting
+# Change one datatype into another datatype
+
+x = '10'
+y = '3'
+
+print(type(x))
+print(type(y))
+print(type(int(x)))
+print(type(int(y)))
+print(int(x)+int(y))
+
+a = 'A'
+b = 'B'
+print(int(a)) # we can't change into integer because a, b are not numbers
+
+z = 10.5
+print(int(z))
+print(float(z))
+
