ab3744a6ce914d04726127208f5ade3fe566afd3
Subha-P-Coder <psubha678@gmail.com>
2025-09-24 08:13:17 +0530

    feat: add tuples examples in 10.tuples.py

---
+++ 10.tuples.py
@@ -0,0 +1,14 @@
+# Immutable
+# order maintain
+# you can use any datatype in a particular variable
+
+trip_summary = ("ubergo", "coimbatore","airport",450.00,"completed","completed")
+
+print(trip_summary)
+print(trip_summary[1])
+
+print(len(trip_summary))
+print(trip_summary.count("completed"))
+
+# print(trip_summary[1]) = "karur" because it is immutable
+
