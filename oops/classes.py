2206543ff215398645769ca8f401a976d01b60d3
Subha-P-Coder <psubha678@gmail.com>
2025-09-25 10:14:17 +0530

    feat: add classes example in classes.py

---
+++ oops/classes.py
@@ -0,0 +1,9 @@
+# class inside function is called method
+# class outside function is called function
+
+
+class student:
+    def say_hello(self):
+        print("i am student")
+s = student()
+s.say_hello()
