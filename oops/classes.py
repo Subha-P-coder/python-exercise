c68cd8d1ca7a15b71608e1ae66ad12abae8e252d
Subha-P-Coder <psubha678@gmail.com>
2025-09-25 10:29:49 +0530

    feat: add classes example in classes.py

--- oops/classes.py
+++ oops/classes.py
@@ -1,9 +1,20 @@
 # class inside function is called method
 # class outside function is called function
 
-
 class student:
     def say_hello(self):
         print("i am student")
 s = student()
 s.say_hello()
+
+class student:
+    def sum(self, a,b):
+        return a+b
+s= student()
+print(s.sum(4,5))
+
+class student:
+    def sum(self, n):
+        return n
+s= student()
+print(s.sum("subha"))
